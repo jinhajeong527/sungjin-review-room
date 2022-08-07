821f9cd97d62907aa82c541d0dd7149ddc07816d
leeseongjeong8 <t01063955526@gmail.com>
2022-08-07 18:32:06 +0900

    add login to getJwt from cookie & add review detail page

--- target/classes/static/main.js
+++ target/classes/static/main.js
@@ -13,6 +13,8 @@ const monthlyBestNext = document.querySelector('main .contents .category #monthl
 const wishListPrev = document.querySelector('main .contents .category #wishList .page__btn__box .prev__page__btn');
 const wishListNext = document.querySelector('main .contents .category #wishList .page__btn__box .next__page__btn');
 
+const showDetailModal = document.querySelector('.review__detail__modal')
+
 let preferredPageNo = 1;
 let monthlyBestPageNo = 1;
 let wishListPageNo = 1;
@@ -132,7 +134,7 @@ function setMainList() {
 function getReviewPreviewHTML(element) {
     return `
     <li class="item">
-    <img src="${element.img}" alt="reviewItem">
+    <img src="${element.img}" alt="reviewItem" class="show__image" onclick="showDetail(${element.id})">
     <span class="item__title">${element.name} <button class="wishlist__btn" onclick="addWishList(${element.id})"><i class="fa-solid fa-cart-shopping"></i></button></span>
     <span class="item__reviews">${element.reviewsCount} views</span>
     </li> 
@@ -196,3 +198,62 @@ function addWishList(showId) {
         alert("위시리스트에 추가되었습니다.")
     })
 }
+
+//todo fill url
+const showDetailUrl = `http://${env.api_address}/...`
+
+function showDetail(showId) {
+    let showDetailPayload = {id: showId} //todo
+    axios.get(showDetailUrl, showDetailPayload)
+    .then(function(response) {
+        console.log(response);
+        openShowDetailModal(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
+
+function openShowDetailModal(showDetailResponse) {
+    let htmlTemplate = getShowDetailModalTemplate(showDetailResponse)
+    showDetailModal.innerHTML = htmlTemplate
+    appendReviewListToShowDetailModal(showDetailResponse.reviewList)
+    let closeBtn = document.querySelector('.review__detail__modal__close__btn')
+    closeBtn.addEventListener('click', closeShowDetailModal)
+    showDetailModal.style.display = 'flex';
+}
+
+function closeShowDetailModal() {
+    showDetailModal.innerHTML = '';
+    showDetailModal.style.display = 'none';
+}
+
+function getShowDetailModalTemplate(showDetailResponse) {
+    return `
+        <div class="review__detail__modal__content">
+            <button class="review__detail__modal__close__btn"><i class="fa-solid fa-xmark"></i></button>
+            <div class="show__info">
+            <img src="${showDetailResponse.imageSrc}" class="show__detail__image">
+            <div class="show__detail__info">
+                <span class="show__detail__title">${showDetailResponse.title}</span>
+                <span class="show__detail__review__cnt">${showDetailResponse.reviewCnt} views</span>
+            </div>
+        </div>
+        <div class="review__content">
+            <span>리뷰</span>
+            <ul class="show__detail__review__list">
+            </ul>
+        </div>
+    `
+}
+
+function appendReviewListToShowDetailModal(reviewList) {
+    let reviewContent = document.querySelector('.review__content .show__detail__review__list');
+    for (let review in reviewList) {
+        let liNode = document.createElement("li")
+        let liText = document.createTextNode(review)
+        liNode.append(liText)
+        reviewContent.appendChild(liNode)
+    }
+}
+
